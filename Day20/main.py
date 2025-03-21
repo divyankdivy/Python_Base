f7fbd42f9b79eeb084b165397dba95e38ebe51ab
Divyank Rajput <119159800+divyankdivy@users.noreply.github.com>
2025-03-21 19:44:57 +0530

    Add files via upload

---
+++ Day20/main.py
@@ -0,0 +1,23 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+machine_off = False
+
+while not machine_off:
+    options = menu.get_items()
+    choice = input(f'What would you like to have?{options}: ')
+    if choice == 'off':
+        machine_off = True
+    elif choice == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
