d1028652936112e4df21ce60b9a91bd164174596
Divyank Rajput <119159800+divyankdivy@users.noreply.github.com>
2025-03-23 21:38:13 +0530

    Add files via upload

---
+++ Day21/quiz_brain.py
@@ -0,0 +1,27 @@
+class QuizBrain:
+    def __init__(self,question_list):
+        self.question_num = 0
+        self.question_list = question_list
+        self.score = 0
+
+    def still_has_questions(self):
+        if self.question_num < len(self.question_list):
+            return True
+        else:
+            return False
+
+    def next_question(self):
+        current_question = self.question_list[self.question_num]
+        self.question_num += 1
+        user_answer = input(f"Q.{self.question_num}: {current_question.text} (True/False): ")
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self,user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print('You Got it right!')
+            self.score += 1
+        else:
+            print("That's Wrong")
+        print(f"The correct answer is {correct_answer}.")
+        print(f"Your score is {self.score}/{self.question_num}")
+        print('\n')
