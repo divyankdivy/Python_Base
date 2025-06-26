e1e4e3dea4db1a0400e92220355e2fa58ff1c24a
Divyank Rajput <119159800+divyankdivy@users.noreply.github.com>
2025-06-26 22:54:14 +0530

    Create Even_and_odd.py

---
+++ Day01/If-else Statements/Even_and_odd.py
@@ -0,0 +1,8 @@
+# Write a program to check whether a number entered by user is even or odd.
+
+num = int(input("Enter a number: "))
+
+if num%2==0:
+  print("This is an even number")
+else:
+  print("This is an odd number")
