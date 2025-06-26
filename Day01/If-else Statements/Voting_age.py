140d76ffba1b92ad80ad5430a8c14487571ff8ff
Divyank Rajput <119159800+divyankdivy@users.noreply.github.com>
2025-06-26 22:50:37 +0530

    Create Voting_age.py

---
+++ Day01/If-else Statements/Voting_age.py
@@ -0,0 +1,10 @@
+print("Hello World")
+
+# Write a Program to check whether a person is eligible for voting or not.(accept age from the user)
+
+age = int(input("Enter your age."))
+
+if age>=18:
+    print("You are eligible for voting")
+else:
+    print("You are not eligible for voting")
