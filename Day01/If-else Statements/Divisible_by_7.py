936891c818ae7916b5ea9e73b36de55070c25e78
Divyank Rajput <119159800+divyankdivy@users.noreply.github.com>
2025-06-26 22:56:54 +0530

    Create Divisible_by_7.py

---
+++ Day01/If-else Statements/Divisible_by_7.py
@@ -0,0 +1,8 @@
+# Write a program to check whether a number is divisible by 7 or not.
+
+num = int(input("Enter a number: "))
+
+if num % 7 == 0:
+  print("This number is divisible by 7")
+else:
+  print("This number is not divisible by 7")
