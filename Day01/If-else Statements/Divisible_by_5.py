ceac45298146c97e8c82c3c83d801ec00145488e
Divyank Rajput <119159800+divyankdivy@users.noreply.github.com>
2025-06-27 23:03:14 +0530

    Create Divisible_by_5.py

---
+++ Day01/If-else Statements/Divisible_by_5.py
@@ -0,0 +1,8 @@
+# Write a program to display "Yes" if a number entered by user is a multiple of five, otherwise print "No".
+
+num = int(input("Enter a number: "))
+
+if num%5==0:
+  print("Yes")
+else:
+  print("No")
