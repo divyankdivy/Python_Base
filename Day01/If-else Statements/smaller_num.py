535c3a98e88f558337d5ad8d6741767ec4515d89
Divyank Rajput <119159800+divyankdivy@users.noreply.github.com>
2025-06-30 12:06:26 +0530

    Create smaller_num.py

---
+++ Day01/If-else Statements/smaller_num.py
@@ -0,0 +1,8 @@
+# Check lowest out of the two numbers
+n1 = int(input('enter number 1: '))
+n2 = int(input('enter number 2: '))
+
+if n1>n2:
+    print('smaller is',n2)
+else:
+    print('smaller is',n1)
