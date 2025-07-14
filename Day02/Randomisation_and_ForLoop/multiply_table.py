d9b21e659f65423ffcce4fb24551decc79c25b94
Divyank Rajput <119159800+divyankdivy@users.noreply.github.com>
2025-07-14 22:51:59 +0530

    Create multiply_table.py

---
+++ Day02/Randomisation_and_ForLoop/multiply_table.py
@@ -0,0 +1,7 @@
+# Write a program to print multiplication table of a given number
+
+number = int(input('enter the number which you want to see the multiplication table: '))
+
+for num in range(1,11):
+    multi = number*i
+    print(f'{number} * {num} = {multi}')
